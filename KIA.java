92252cb7d19bbfc937717d36c4077e32dd2d93f2
ahmed <0ahmadsailm0@gmail.com>
2024-05-28 10:30:48 +0300

    add new class kia

---
+++ KIA.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ahmed_cars;
+
+/**
+ *
+ * @author Ahmed
+ */
+public class KIA {
+    
+}
